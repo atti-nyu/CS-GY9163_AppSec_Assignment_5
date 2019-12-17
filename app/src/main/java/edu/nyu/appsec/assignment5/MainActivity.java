fad4d164a577a3fd0b9cbe5160063812e5ccb57b
atti-nyu <am9589@nyu.edu>
2019-12-17 00:40:44 -0500

    removed additional vulns

--- app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
+++ app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
@@ -1,29 +1,14 @@
 package edu.nyu.appsec.assignment5;
-
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
-import android.net.http.SslError;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.SerializablePermission;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 
     private static final String SPELL_CHECK_URL = "http://appsecclass.report:8080/";
     private static final String KNOWN_HOST = "appsecclass.report";
@@ -44,22 +29,6 @@ import java.net.URL;
         }
     }
 
-    /* Get location data to provide language localization
-    *  Supported languages ar-DZ zh-CN en-US en-IN en-AU fr-FR
-    */
-    @Override
-
-
-    /* Necessary to implement the LocationListener interface
-    */
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {}
-
-    @Override
-    public void onProviderEnabled(String s) {}
-
-    @Override
-    public void onProviderDisabled(String s) {}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,9 +39,7 @@ import java.net.URL;
         view.setWebViewClient(new MyWebViewClient());
 
         WebSettings settings = view.getSettings();
-        settings.setAllowFileAccessFromFileURLs(true);
         settings.setJavaScriptEnabled(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
 
        
         setContentView(view);
